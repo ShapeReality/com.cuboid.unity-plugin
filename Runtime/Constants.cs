51d4b05a9d34dc5b727e0d19863b47df19f098b3
Arjo Nagelhout <arjo.nagelhout@gmail.com>
2023-05-16 23:28:07 +0200

    added constants

--- Runtime/Constants.cs
+++ Runtime/Constants.cs
@@ -6,6 +6,14 @@ namespace Cuboid.UnityPlugin
 {
     public static class Constants
     {
-        public const int k_Test = 5;
+        public const int k_ThumbnailSize = 256;
+
+        public const string k_ThumbnailEntryName = "thumbnail.png";
+
+        public const string k_DocumentFileExtension = ".cuboid"; // with dot
+        public const string k_DocumentEntryName = "document.json";
+
+        public const string k_AssetCollectionFileExtension = ".zip";
+        public const string k_AssetCollectionEntryName = "collection.json";
     }
 }
